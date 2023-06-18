045b941b4a758a7a2da87b1cd3c51bc5a8646664
Ernesto Vences <nyxernesto2@gmail.com>
2023-06-18 00:07:32 -0500

    Guarda el archivo localmente con su extensión correctamente

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,14 +1,30 @@
 import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import path, { extname, join } from 'path';
+import * as fs from 'fs-extra';
 
 @Controller('documents')
 export class DocumentsController {
 
     @Post('upload')
     @UseInterceptors(FileInterceptor('pdf'))
-    uploadFile(@UploadedFile() file: Express.Multer.File) {
-        console.log(file);
+    async uploadFile(@UploadedFile() file: Express.Multer.File) {
+        const originalPDFName = file.originalname;
+        const extension = extname(originalPDFName);
 
-        return { message: 'File uploaded successfully' }
+        try {
+            const fileName = `${Date.now()}${extension}`;
+
+            const filePath = join(__dirname, '..', '..', 'uploads', fileName);
+
+            await fs.move(file.path, filePath);
+
+            console.log(filePath);
+
+            return { message: 'File uploaded successfully' };
+        } catch (error) {
+            console.log(error);
+            throw new Error('Ocurrió un error al guardar el archivo PDF.');
+        }
     }
 }
