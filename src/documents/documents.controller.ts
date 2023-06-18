c55effd5cef2d2ee2c9d23ad9cc7c19c1b6e2dc6
Ernesto Vences <nyxernesto2@gmail.com>
2023-06-17 23:55:50 -0500

    Ruta /documents/upload recibe el archivo

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('documents')
-export class DocumentsController {}
+export class DocumentsController {
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('pdf'))
+    uploadFile(@UploadedFile() file: Express.Multer.File) {
+        console.log(file);
+
+        return { message: 'File uploaded successfully' }
+    }
+}
--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express/multer';
 import { DocumentsController } from './documents.controller';
 
 @Module({
-  controllers: [DocumentsController]
+  controllers: [DocumentsController],
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
 })
 export class DocumentsModule {}
